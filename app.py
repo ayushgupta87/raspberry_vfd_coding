2d1c6f408fa43e04fcd945d605922b2a80ecbaf2
ayushohlocal <ayushgupta@ohlocal.net>
2025-02-14 23:37:41 +0530

    initialize

--- app.py
+++ app.py
@@ -164,4 +164,4 @@ if __name__ == '__main__':
     # mqtt_thread.start()
 
     print("🔄 Starting Flask Modbus RTU API...")
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
