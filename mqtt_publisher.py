b47a25330a2cfc8699410b72315c3deb000c386e
ayushohlocal <ayushgupta@ohlocal.net>
2025-02-15 02:44:39 +0530

    initialize

--- mqtt_publisher.py
+++ mqtt_publisher.py
@@ -76,7 +76,7 @@ def publish_modbus_to_mqtt():
                 print(f"📡 Published Frequency {amps}A to {MQTT_TOPIC}")
             else:
                 print(f"⚠️ Modbus Read Failed: {data.get('error')}")
-        time.sleep(120)
+        time.sleep(5)
 
 if __name__ == "__main__":
     print("🔄 Starting MQTT Publisher...")
